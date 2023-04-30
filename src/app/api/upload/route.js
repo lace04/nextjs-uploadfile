99b28d1228f16266e1f1fc743717a8ee0686d87b
lace04 <luisace04@gmail.com>
2023-04-30 15:40:07 -0500

    add style with tailwindcss

---
+++ src/app/api/upload/route.js
@@ -0,0 +1,27 @@
+import { writeFile } from 'fs/promises';
+import { NextResponse } from 'next/server';
+import path from 'path';
+
+export async function POST(request) {
+  try {
+    const data = await request.formData();
+    const file = data.get('file');
+
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+
+    const filePath = path.join(process.cwd(), 'public', 'uploads', file.name);
+
+    writeFile(filePath, buffer);
+
+    console.log('Guardado en ', filePath);
+    return new Response(JSON.stringify({ message: 'Uploading file!' }));
+  } catch (error) {
+    return NextResponse.json(
+      JSON.stringify({
+        message: 'No file selected',
+      }),
+      { status: 400 }
+    );
+  }
+}
